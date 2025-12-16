704383f49721c7a77c3e140fc671de9eea97155f
alfaro <abittori@gmail.com>
2025-12-16 15:29:04 +0100

    Ajout de Sequelize et du modèle Joke

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const app = require("./app");
+const sequelize = require("./config/database");
+require("./models/Joke");
+
 const port = 3001;
 
-app.listen(port, () => {
-  console.log(`API listening on port ${port}`);
-});
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`API listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
