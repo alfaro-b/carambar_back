7dc90dff394986cdcb9f1ffa82475e2a6c493661
alfaro <abittori@gmail.com>
2025-12-18 10:53:11 +0100

    Ajout de Swagger pour la documentation

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const jokeRoutes = require("./routes/joke.routes");
 
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs/swagger.json');
+
 app.use(express.json()); // doc Express: built-in middleware express.json()
 
 app.use("/api/v1", jokeRoutes); // versionning demandé
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Api documentée (Swagger demandé) 
 
 module.exports = app;
